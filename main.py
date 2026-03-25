979d8e05d4ab91459f6e881ed467372d70a4329b
Stravis1994 <sean.travis1994@gmail.com>
2026-03-25 12:46:22 +1100

    Changes to be committed: 	deleted:    Website/styles.css 	modified:   main.py 	modified:   pyproject.toml 	renamed:    Website/scripts.js -> static/scripts.js 	new file:   static/styles.css 	renamed:    Website/index.html -> templates/index.html 	new file:   uv.lock

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from cst-150-project-3!")
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
